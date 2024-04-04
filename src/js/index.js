8c71b8f2b110bcf672a97e4567fb50d75243dbe4
Israel Pina <99025898+IsraelPina32@users.noreply.github.com>
2024-04-04 01:48:15 -0300

    style : change my html and javascript

--- src/js/index.js
+++ src/js/index.js
@@ -2,17 +2,18 @@ const botao = document.querySelector('.btn');
 const formulario = document.getElementById('form');
 const inputEmail = document.querySelector('input');
 
+
 function validarEmail(email) {
-    // Expressão regular para validar email
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
 }
 
 formulario.addEventListener("submit", (event) => {
+
     event.preventDefault()
 
-    
     let campoObrigatorioDandoError = "Please provide a valid email"
+
         if(inputEmail.value === "" || !validarEmail(inputEmail.value)){
             inputEmail.classList.add("error");
             botao.nextElementSibling.textContent = campoObrigatorioDandoError; 
